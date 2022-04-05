96fac6de2cfc9b99d81a54a5893bf7e764edf1d7
Oriel Vinci Absin <oriel.absin@gmail.com>
2022-04-05 15:32:28 +0800

    :ambulance: fix: static environment variables

--- client/src/constants/index.tsx
+++ client/src/constants/index.tsx
@@ -1,6 +1,6 @@
 const constants = {
   apiUrl:
-    process.env.NEXT_PUBLIC_APIURL || 'https://exptrackr-api.herokuapp.com',
+    process.env.NEXT_PUBLIC_APIURL || 'https://exptrackr-api.herokuapp.com/api',
 }
 
 export default constants
